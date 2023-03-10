cf561b31b13f456563a4b8ff2db9f594de9d9c7c
Rodrigo N Giraudo <rodrigogiraudo@gmail.com>
2023-03-10 20:07:21 +0100

    feat(monorepo): after build

---
+++ apps/docs/.next/static/gfcUhHCNGuLKdU-Oc5GbT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5a54aa276bd2a3d9.js"],"/_error":["static/chunks/pages/_error-1e40f0410f825722.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ apps/web/.next/static/Z3ItyytjLlL2IMUNEbqD2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e8cc7dd2a73193b4.js"],"/_error":["static/chunks/pages/_error-1e40f0410f825722.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
