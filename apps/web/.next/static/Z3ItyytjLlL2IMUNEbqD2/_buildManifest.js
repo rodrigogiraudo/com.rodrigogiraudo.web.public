0ef70b2623f96b9f16bc57971d29787a5db2e4b1
Rodrigo Nicolás Giraudo <rodrigogiraudo@gmail.com>
2023-03-11 19:07:10 +0100

    Delete apps/web/.next directory

--- apps/web/.next/static/Z3ItyytjLlL2IMUNEbqD2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e8cc7dd2a73193b4.js"],"/_error":["static/chunks/pages/_error-1e40f0410f825722.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
